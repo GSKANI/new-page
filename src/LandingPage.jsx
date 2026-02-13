35dddbd99bdd37f2359f0b9689133f543bbed019
thenavya2606 <navyapriyadarsini3@gmail.com>
2026-02-13 14:05:25 +0530

    facebook

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -67,6 +67,15 @@ const LandingPage = () => {
                             </div>
                             <span className="social-text">LinkedIn</span>
                         </a>
+
+                        <a href="https://www.facebook.com/share/1Gw3V4ashV/" className="social-link" target="_blank" rel="noopener noreferrer">
+                            <div className="social-icon">
+                                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                                </svg>
+                            </div>
+                            <span className="social-text">Facebook</span>
+                        </a>
                     </div>
                 </div>
             </div>
